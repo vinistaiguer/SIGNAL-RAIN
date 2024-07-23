9e2f0429da752159169f9ce55c2187bed5889e3d
vinicius.santos <vinicius.santos@icolabora.com>
2024-07-23 16:19:09 -0300

    service changed to run cron every fifteen days

--- src/routes/weatherRoutes.ts
+++ src/routes/weatherRoutes.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import { getWeatherController } from "../controllers/weatherController";
-import { weatherControllerFiveDays } from "../controllers/weatherControllerFiveDays";
 
 const router = express.Router();
 
 router.post("/weather", getWeatherController);
-router.post("/weather-five-days", weatherControllerFiveDays);
 
 export default router;
